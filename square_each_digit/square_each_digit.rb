02e07175603cadf6200f35ea97b65354449c9d9a
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-19 16:25:54 +0545

    Square_each_digit is completed

--- square_each_digit/square_each_digit.rb
+++ square_each_digit/square_each_digit.rb
@@ -1,5 +1,13 @@
 def square_each_digit num
   # Write your code here
+  num_array=num.to_s.split('').map{|i| i.to_i}
+  sq=[]
+
+  num_array.each do |i|
+    sq.push(i*i)
+  end
+  
+  sqr=sq.join 
 end
 
 describe "#square_each_digit" do
@@ -18,3 +26,29 @@ describe "#square_each_digit" do
     end
   end
 end
+# n=12322
+# s = n.to_s.split('').map {|i| i.to_i}
+# sq=[]
+# # x=s.to_i 
+# # puts s[0].class 
+# # puts s 
+# s.each do |i|
+#   sq.push(i*i)
+# end
+# sqr=sq.join
+# puts sqr 
+
+# puts s 
+# puts s.class 
+# # a=[]
+# i=0
+# while i<n.length
+#   a.push(n[i])
+# end
+# puts a 
+
+# a=n
+# puts a.class 
+# a.each do |i|
+#   puts i*i
+# end
